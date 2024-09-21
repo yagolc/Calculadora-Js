230fcccb388702ad185114906bb6c13dfac38010
yagolc <yagolimacapistrano@alu.ufc.br>
2024-09-21 16:00:04 -0300

    CriandoFunçãoFazerOperacão

--- script.js
+++ script.js
@@ -11,3 +11,7 @@ function clearDisplay() {
 function deleteLast() {
     document.getElementById("display").value = document.getElementById("display").value.slice(0, -1);
 }
+
+function calculate() {
+    document.getElementById("display").value = eval(document.getElementById("display").value);
+}
