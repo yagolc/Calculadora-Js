103f93f2e731a117150a64e90c41b4f9f9fde307
yagolc <yagolimacapistrano@alu.ufc.br>
2024-09-21 15:53:53 -0300

    CriandoFunçãoDeletarUltimoCaractere

--- script.js
+++ script.js
@@ -7,3 +7,7 @@ function appendToDisplay(value) {
 function clearDisplay() {
     document.getElementById("display").value = "";
 }
+
+function deleteLast() {
+    document.getElementById("display").value = document.getElementById("display").value.slice(0, -1);
+}
